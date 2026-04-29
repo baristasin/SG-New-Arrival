6e90a1ab731aff0075a703a52ecd78277a15eb75
Baris <>
2026-04-29 15:02:45 +0200

    DistanceUtil X-Z

---
+++ Assets/Game/Scripts/Utilities/DistanceUtil.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public static class VectorUtils
+{
+    /// <summary>
+    /// Returns distance value not squared, based on X-Z (Y is not considered)
+    /// </summary>
+    public static float DistanceXZ(Vector3 a, Vector3 b)
+    {
+        float dx = a.x - b.x;
+        float dz = a.z - b.z;
+        return (dx * dx) + (dz * dz);
+    }
+
+    /// <summary>
+    /// Squared real distance value, heavy on CPU so, careful usage
+    /// </summary>
+    public static float DistanceXZActual(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(DistanceXZ(a, b));
+    }
+}
