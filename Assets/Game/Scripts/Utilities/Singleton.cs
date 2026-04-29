61b5255ebd69f16095afc40b035196e1ce9a4562
Baris <>
2026-04-29 15:02:14 +0200

    Generic singleton

---
+++ Assets/Game/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Game.Scripts.Utilities
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {        
+        private static T _instance;
+        private static readonly object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = FindFirstObjectByType<T>();
+                        if (_instance == null)
+                        {
+                            GameObject singletonObject = new GameObject(typeof(T).Name);
+                            _instance = singletonObject.AddComponent<T>();
+                        }
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
