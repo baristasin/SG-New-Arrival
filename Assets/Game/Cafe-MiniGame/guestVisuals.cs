5d60d6f22d47bf2a8d51aab67a4713365595511c
smarty-304 <91762850+smarty-304@users.noreply.github.com>
2026-05-25 16:00:43 +0200

    renamed Guestvisuals with UpperCamalCase

--- Assets/Game/Cafe-MiniGame/guestVisuals.cs
+++ Assets/Game/Cafe-MiniGame/guestVisuals.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class guestVisuals : MonoBehaviour
+public class GuestVisuals : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
